91b8fe9769c5aecff9d950c06b0ee1e4d072cf98
Lisa Chandra <liza761c@gmail.com>
2020-05-22 14:31:44 +0530

    changed resume and pic and toggle icon

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,8 @@
 // console.log("HELLO");
 
 var toggleLight = document.querySelector(".toggle-light");
-var toggleLightIcon = document.querySelector("#toggle-light");
+// var toggleLightIcon = document.querySelector("#toggle-light");
+var toggleLightIconSlider = document.querySelector("#toggle-light-slider");
 var main = document.querySelector("#main");
 var header = document.querySelector(".header-title");
 var footer = document.querySelector("#bottom-bar");
@@ -11,46 +12,21 @@ var tags = document.querySelectorAll(".tag");
 
 toggleLight.addEventListener("click", function() {
 	// console.log("CLICKED!");
-	toggleLightIcon.classList.toggle("fa-sun");
-	toggleLightIcon.classList.toggle("fa-moon");
+	// toggleLightIcon.classList.toggle("fa-sun");
+	// toggleLightIcon.classList.toggle("fa-moon");
+	toggleLightIconSlider.classList.toggle("fa-toggle-off");
+	toggleLightIconSlider.classList.toggle("fa-toggle-on");
 	var day = sessionStorage.getItem('day')
 	if(!day || day == 'true') {
 		sessionStorage.setItem('day', 'false');
 	} else {
 		sessionStorage.setItem('day', 'true');
 	}
-	// if(toggleLightIcon.classList.contains("fa-moon")) {
-	// if(sessionStorage.getItem('day') == "false") {
-	// 	console.log("YESS");
-	// 	main.style.backgroundColor = "#181818";
-	// 	header.style.backgroundColor = "#181818";
-	// 	if(footer) footer.style.backgroundColor = "#181818";
-	// 	code.forEach(function(cod) {
-	// 		if(cod.classList.contains("codeblock")) {
-	// 			cod.style.backgroundColor = "#9eabb3";
-	// 		}
-	// 		else {
-	// 			cod.style.backgroundColor = "rgb(21, 32, 43)";
-	// 		}
-	// 	});
-	// 	bottomActions.forEach(function(actions) {
-	// 		actions.style.backgroundColor = "rgb(21, 32, 43)";
-	// 	});
-	// 	tags.forEach(function(tag) {
-	// 		tag.style.backgroundColor = "rgb(21, 32, 43)";
-	// 	});
-	// } else {
-	// 	// #f7f8f8
-	// 	main.style.backgroundColor = "#fff";
-	// 	header.style.backgroundColor = "#fff";
-	// 	footer.style.backgroundColor = "#fff";
-	// 	code.forEach(function(cod) {
-	// 		cod.style.backgroundColor = "#f7f8f8";
-	// 	});
-	// }
 });
 setInterval(function() { 
 	if(sessionStorage.getItem('day') == "false") {
+		toggleLightIconSlider.classList.remove("fa-toggle-off");
+		toggleLightIconSlider.classList.add("fa-toggle-on");
 		main.style.backgroundColor = "#181818";
 		header.style.backgroundColor = "#181818";
 		if(footer) footer.style.backgroundColor = "#181818";
@@ -69,6 +45,8 @@ setInterval(function() {
 			tag.style.backgroundColor = "rgb(21, 32, 43)";
 		});
 	} else {
+		toggleLightIconSlider.classList.add("fa-toggle-off");
+		toggleLightIconSlider.classList.remove("fa-toggle-on");
 		main.style.backgroundColor = "#fff";
 		header.style.backgroundColor = "#fff";
 		if(footer) footer.style.backgroundColor = "#fff";
@@ -83,6 +61,3 @@ setInterval(function() {
 		});
 	}
 }, 100);
-// setInterval(function() { 
-// 	console.log(sessionStorage.getItem('day'));
-// }, 1000);
